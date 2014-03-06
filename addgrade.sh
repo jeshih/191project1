418e71ba6183fb250e547017c24925d5b974ac2f
Jeffrey W Shih <jeshih@plus.seas.upenn.edu>
2014-03-06 03:28:20 -0500

    fixed add grade - ignoring the space issue

--- addgrade.sh
+++ addgrade.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-validass=$(grep -c $2 grader.csv)
-validstu=$(grep -c $1 grader.csv)
+validass=$(grep -c -w $2 grader.csv)
+validstu=$(grep -c -w $1 grader.csv)
 
 if [ $validstu -ne 1 ]
 then
@@ -9,25 +9,27 @@ then
 elif [ $validass -ne 1 ]
 then
     echo "ASSIGNMENT NOT IN SYSTEM"
-elif [ $# == 2 ]
+elif [ $# -eq 2 ]
 then
     echo "Enter student score for assignment: "
     read input_variable
     student=$(grep -n $1 grader.csv | cut -f1 -d:)
-#    sed -i ''$student' s/0/'$input_variable'/'  grader.csv
+    stuline=$(sed -n ''$student'p' grader.csv)
 
-    IFS=", " 
+    fline=$(sed -n '1p' grader.csv) #firstline
+
+    IFS=", "
+    read -a line1 <<< "$fline"
+    read -a linestu <<< "$stuline"
     count=0
-    firstline=$(head -1 grader.csv)
-    for i in $firstline; do
-	let count=count+1
-	if [[ $i = $2 ]]
+    for e in ${line1[@]}; do
+	count=$(($count + 1))
+	if [[ $e = $2 ]]
 	then
 	    break
 	fi
     done
-    unset IFS
-    sed -i ''$student's/[^ ]*/'$input_variable'/'$count'' grader.csv
+    sed -i ''$student's/[^,]*/'$input_variable'/'$count'' grader.csv
  else
     echo "Invalid Input!!"
 fi
--- makefile
+++ makefile
@@ -2,3 +2,5 @@
 rm grader.csv
 echo PENNKEY, LAST_NAME, FIRST_NAME >> grader.csv
 echo PENNKEY, LAST_NAME, FIRST_NAME >> grader.csv
+bash addstudent.sh jeshih shih jeff
+bash addstudent.sh claurent laurent charles
